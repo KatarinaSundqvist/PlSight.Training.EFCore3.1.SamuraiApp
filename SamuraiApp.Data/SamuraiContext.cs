5b4256df4badf8702f315a1a9578929f2698cce3
KatarinaSundqvist <singsongkat@live.com>
2020-02-26 15:51:11 +0100

    Added one-to-one relationship samurais-horses, and added new migration and updated database

--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -6,6 +6,7 @@ namespace SamuraiApp.Data {
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Quote> Quotes { get; set; }
         public DbSet<Clan> Clans { get; set; }
+        public DbSet<Battle> Battles { get; set; }
 
         // Hard-coded connection string. Just for demo and first looks!
         // For real software, use dependency injection
@@ -17,6 +18,11 @@ namespace SamuraiApp.Data {
         // the many-to-many relationship between Battles and Samurais
         protected override void OnModelCreating(ModelBuilder modelBuilder) {
             modelBuilder.Entity<SamuraiBattle>().HasKey(s => new { s.SamuraiId, s.BattleId });
+            modelBuilder.Entity<Horse>().ToTable("Horses");
+            // EF Core will find the Horse class without it having a DbSet property
+            // because it's linked to the Samurai class
+            // but it would name the table Horse, so we change that here
+            // (our business rules don't want anyone to be able to interact directly with the horse class, so we don't want to expose it via DbSet)
         }
     }
 }
