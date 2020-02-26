5843cdb321e3f859b5e6c1bea86d48424ff813d3
KatarinaSundqvist <singsongkat@live.com>
2020-02-26 11:54:15 +0100

    Added a context class and a simple demo for interacting with the database

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,9 +1,36 @@
-﻿using System;
+﻿using SamuraiApp.Data;
+using SamuraiApp.Domain;
+using System;
+using System.Linq;
 
 namespace ConsoleApp {
-    class Program {
+    internal class Program {
+
+        // NOTE: This is just for demo, very simple! 
+        // NOT best practise, not recommended for use in real software!
+
+        private static SamuraiContext context = new SamuraiContext();
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            context.Database.EnsureCreated();
+            GetSamurais("Before Add:");
+            AddSamurai();
+            GetSamurais("After Add:");
+            Console.WriteLine("Press any key");
+            Console.ReadKey();
+        }
+
+        private static void AddSamurai() {
+            var samurai = new Samurai { Name = "Sampson" };
+            context.Samurais.Add(samurai);
+            context.SaveChanges();
+        }
+
+        private static void GetSamurais(string text) {
+            var samurais = context.Samurais.ToList();
+            Console.WriteLine($"{text}: Samurai count is {samurais.Count}");
+            foreach (var samurai in samurais) {
+                Console.WriteLine(samurai.Name);
+            }
         }
     }
 }
---
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+
+namespace SamuraiApp.Data {
+    public class SamuraiContext : DbContext {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Clan> Clans { get; set; }
+
+        // Hard-coded connection string. Just for demo and first looks!
+        // For real software, use dependency injection
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseSqlServer("Server = (localdb)\\mssqllocaldb; Database = SamuraiAppDataCore");
+        }
+    }
+}
