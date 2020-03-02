9464f76253df81c23c6ebd1a6494761440df4ed8
KatarinaSundqvist <singsongkat@live.com>
2020-03-02 16:10:06 +0100

    Queries against our keyless entity

--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -8,6 +8,7 @@ namespace SamuraiApp.Data {
         public DbSet<Quote> Quotes { get; set; }
         public DbSet<Clan> Clans { get; set; }
         public DbSet<Battle> Battles { get; set; }
+        public DbSet<SamuraiBattleStat> SamuraiBattleStats { get; set; }
 
         public static readonly ILoggerFactory ConsoleLoggerFactory = LoggerFactory.Create(builder => {
             builder
@@ -37,6 +38,10 @@ namespace SamuraiApp.Data {
             // because it's linked to the Samurai class
             // but it would name the table Horse, so we change that here
             // (our business rules don't want anyone to be able to interact directly with the horse class, so we don't want to expose it via DbSet)
+            modelBuilder.Entity<SamuraiBattleStat>().HasNoKey().ToView("SamuraiBattleStats");
+            // the first part shows that it is intentional that this entity doesn't have a key
+            // The second part is to tell the application that there is already a view for this entity. Otherwise, the next migration we apply will try to create a table
+            // HasNoKey entities will never be tracked by EF Core, and if you try to force it with AsTracking, EF Core will ignore your attempt
         }
     }
 }
---
+++ SamuraiApp.Domain/SamuraiBattleStat.cs
@@ -0,0 +1,7 @@
+﻿namespace SamuraiApp.Domain {
+    public partial class SamuraiBattleStat {
+        public string Name { get; set; }
+        public int? NumberOfBattles { get; set; }
+        public string EarliestBattle { get; set; }
+    }
+}
